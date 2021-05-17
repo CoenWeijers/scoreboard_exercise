c4bb9e7705e9e955d123ea28124996a409b5da94
Coen <weco110503@leerling.mosa-rt.be>
2021-05-17 15:33:30 +0200

    if functies toegevoegd 32 testen  werkend

--- solution_function.py
+++ solution_function.py
@@ -14,8 +14,23 @@ def update_score(oldscore, team):
 
     # code
 
-if score == "h"
+    if  team == "H":
+        points_home = points_home +1
 
-    set_home
+    if team == "A":
+       points_away = points_away  +1
+
+    if points_away >=25  :
+       points_away = 0
+       sets_away = sets_away +1
+       points_home = 0
+
+    if points_home >=25:
+       points_home = 0
+       sets_home = sets_home +1
+       points_away = 0
+
+
+
 
     return sets_home, sets_away, points_home, points_away
